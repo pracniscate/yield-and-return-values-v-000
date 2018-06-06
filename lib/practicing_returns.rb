839a42a7a4aba9b79d585a6a51b9957a2ba05373
Lorena <lorena.giddens@outlook.com>
2018-06-06 02:03:50 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,15 @@
-require 'pry'
-
 def hello(array)
   i = 0
+  # set a var collection to an empty array
+  collection = []
   while i < array.length
-    yield(array[i])
+    # push the return value into the collection array
+    collection << yield(array[i])
     i += 1
   end
+  # return the new collection at the end of the method
+  collection
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
